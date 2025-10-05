ed046291f7e9fe70baa8b3f1e98523436767315d
Naomi Bllstr <naomi.ballestero.02@gmail.com>
2025-10-05 17:06:58 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const showalertbtn= document.getElementById("showalertbtn");
+const alert= document.getElementById("alert");
+const confirmbtn=document.getElementById("confirmbtn");
+
+showalertbtn.addEventListener("click", function() {
+    alert.style.display= "flex";
+});
+
+confirmbtn.addEventListener("click", function() {
+    alert.style.display= "none";
+});
+
